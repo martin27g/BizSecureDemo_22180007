4d1d349c25e128584735eaa60007270a818bde97
Martin Deshev <azsampas8@gmail.com>
2026-03-04 16:54:31 +0200

    Add project files.

---
+++ BizSecureDemo_22180007/ViewModels/CreateOrderVm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BizSecureDemo_22180007.ViewModels;
+
+public class CreateOrderVm
+{
+    [Required, MaxLength(80)]
+    public string Title { get; set; } = "";
+    [Required]
+    public decimal Amount { get; set; }
+}
+
---
+++ BizSecureDemo_22180007/ViewModels/LoginVm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BizSecureDemo_22180007.ViewModels;
+
+public class LoginVm
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = "";
+    [Required]
+    public string Password { get; set; } = "";
+}
+
